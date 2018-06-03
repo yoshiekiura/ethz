14793c418a09da4905f132285a4c4b7783f7f3ef
chen zhihao <chenzhihao@eenet.com>
2018-06-03 23:54:02 +0800

    订单列表

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,27 +9,19 @@
     <meta http-equiv = "X-UA-Compatible" content = "IE=edge,chrome=1" />
     <meta name="renderer" content="webkit">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
-    <title>发财致富之路</title>
+    <title>{{config('site_name')}} 发财致富之路</title>
     <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
     <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}" />
     <script type="text/javascript">
     	const hostUrl = '{{ env("APP_URL") }}';
-        @if (isset($signPackage))
-            const wxConfig = {
-                appId: '{{ $signPackage['appId'] }}',
-                timestamp: '{{ $signPackage['timestamp'] }}',
-                nonceStr: '{{ $signPackage['nonceStr'] }}',
-                signature: '{{ $signPackage['signature'] }}',
-            };
-        @endif
     </script>
 </head>
 <body>
-<link rel="stylesheet" href="{{ asset('css/style.css') }}" />
+<link rel="stylesheet" href="{{ asset('css/style.css') }}?v={{ env('APP_VERSION') }}" />
 <div id="app">
     <app></app>
 </div>
 
-<script src="{{ asset('js/app.js') }}?v={{ time() }}"></script>
+<script src="{{ asset('js/app.js') }}?v={{ env('APP_VERSION') }}"></script>
 </body>
 </html>
