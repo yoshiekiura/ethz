08719e288bdfe3d35802a6658e5653d4d4a0180c
chen zhihao <chenzhihao@eenet.com>
2018-06-11 22:51:38 +0800

    币价添加美金

--- app/Http/Controllers/Api/TickerController.php
+++ app/Http/Controllers/Api/TickerController.php
@@ -10,14 +10,16 @@ class TickerController extends Controller
 
     public function index(Request $request)
     {
-        $client = new \GuzzleHttp\Client(['verify' => false]);
-        $response = $client->request('GET', 'https://api.binance.com/api/v1/ticker/24hr', [
-            'query' => [
-                'symbol' => 'ETHUSDT',
-            ],
-        ]);
+        // $client = new \GuzzleHttp\Client(['verify' => false]);
+        // $response = $client->request('GET', 'https://api.binance.com/api/v1/ticker/24hr', [
+        //     'query' => [
+        //         'symbol' => 'ETHUSDT',
+        //     ],
+        // ]);
 
-        $ticker = json_decode((string)$response->getBody());
+        // $ticker = json_decode((string)$response->getBody());
+        $ticker = json_decode('{"openPrice":"567.00000000","lastPrice":"522.00000000","highPrice":"568.45000000","lowPrice":"501.13000000","volume":"210881.66080000"}
+');
         return $this->responseSuccess([
             'open' => $ticker->openPrice,
             'last' => $ticker->lastPrice,
